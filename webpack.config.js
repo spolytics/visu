4b2c111e89e8d26ea814a794ec116f446958c324
Mirco Zeiss <mirco.zeiss@gmail.com>
2017-03-20 15:57:09 +0100

    replace make and browserify with webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+
+const path = require('path')
+
+module.exports = {
+  entry: './docs/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'docs')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './docs'
+  }
+}
