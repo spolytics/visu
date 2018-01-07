791de9828b68bc7fd86b1c455b891d0514c20462
Mirco Zeiss <mirco.zeiss@gmail.com>
2018-01-07 22:18:31 +0100

    add highlight and clear

--- docs/court.js
+++ docs/court.js
@@ -16,10 +16,27 @@ export default class CourtComponent extends React.Component {
     this.court.render()
   }
 
+  onClick = () => {
+    this.court.clear()
+  }
+
+  highlight = event => {
+    this.court.highlight(event.target.name)
+  }
+
   render () {
     return (
       <section>
         <svg ref='svg' />
+        <button type='button' name='b9' onClick={this.highlight}>
+          highlight zone b9
+        </button>
+        <button type='button' name='a3' onClick={this.highlight}>
+          highlight zone a3
+        </button>
+        <button type='button' onClick={this.onClick}>
+          clear
+        </button>
       </section>
     )
   }
